afd867a20cfaa0f89fba5ce0797b7e3abaebf489
Royal <royaldev1031@gmail.com>
2024-03-11 06:49:45 -0400

    menu of child page links

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,6 +1,18 @@
 <?php 
   get_header(); ?>
 
+<?php
+  function doubleMe($x) {
+    return $x * 2;
+  }
+
+  function tripleMe($x) {
+    return $x * 3;
+  }
+
+  echo tripleMe(doubleMe(5));
+?>
+
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
       <div class="page-banner__content container t-center c-white">
