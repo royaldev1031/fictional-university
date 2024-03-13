01b2fccf46c4c55891df7cbce289f8babacce7f1
Royal <royaldev1031@gmail.com>
2024-03-12 22:45:40 -0400

    custom queries

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -32,7 +32,7 @@
               <span class="event-summary__day">02</span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
+              <h5 class="event-summary__title headline headlinel--tiny"><a href="#">Quad Picnic Party</a></h5>
               <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
             </div>
           </div>
@@ -43,27 +43,26 @@
       <div class="full-width-split__two">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+          <?php
+            $homepagePosts = new WP_Query(array(
+              'posts_per_page' => 2
+            ));
 
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Jan</span>
-              <span class="event-summary__day">20</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-              <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
+            while ($homepagePosts->have_posts()) {
+              $homepagePosts->the_post(); ?>
+              <div class="event-summary">
+                <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                  <span class="event-summary__month"><?php the_time('M')?></span>
+                  <span class="event-summary__day"><?php the_time('d')?></span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                  <p><?php echo wp_trim_words( get_the_content(), 18 ); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                </div>
+              </div>
+            <?php }
+            wp_reset_postdata(  );
+          ?>
 
           <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
