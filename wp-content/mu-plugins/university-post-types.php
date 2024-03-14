6d43aeaa9e0a2779d0993d4d9f335d6e1c6ed4aa
Royal <royaldev1031@gmail.com>
2024-03-14 07:09:24 -0400

    create professor post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -33,6 +33,21 @@
       ),
       'menu_icon' => 'dashicons-awards'
     ));
+
+    // Professor Post Type
+    register_post_type('professor', array(
+      'supports' => array('title', 'editor'),
+      'public' => true,
+      'show_in_rest' => true,
+      'labels' => array(
+        'name' => 'Professors',
+        'add_new_item' => 'Add New Professor',
+        'edit_item' => 'Edit Professor',
+        'all_items' => 'All Professors',
+        'singular_name' => 'Professor'
+      ),
+      'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
   }
 
   add_action('init', 'university_post_types');
